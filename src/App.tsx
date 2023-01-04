1e12a8e9983a6f4dc903a6cb936cac8d46fa7e26
Dariannyko <dariannyko@gmail.com>
2023-01-04 21:25:45 +0300

    fix: add Links Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,52 @@
 import { Navbar } from "@/scenes/Navbar/Navbar";
-import {Home} from "@/scenes/Home/Home"
+import { Home } from "@/scenes/Home/Home";
 import { Benefits } from "@/scenes/Benefits/Benefits";
 import { OurClasses } from "@/scenes/OurClasses/OurClasses";
 import { ContactUs } from "./scenes/ContactUs/ContactUs";
 import { Footer } from "./scenes/Footer/Footer";
 import { useState, useEffect } from "react";
+import { NavigationLinksType } from "./shared/Types";
 
-export const navigationLinks: {
-  home: string;
-  benefits: string;
-  ourClasses: string;
-  contactUS: string;
-} = {
+export const navigationLinks: NavigationLinksType = {
   home: "home",
   benefits: "benefits",
   ourClasses: "ourclasses",
   contactUS: "contactus",
 };
 
-
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home');
+  const [selectedPage, setSelectedPage] = useState<string>("home");
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-  
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
         setIsTopOfPage(true);
-        setSelectedPage(navigationLinks.home)
+        setSelectedPage(navigationLinks.home);
       }
 
       if (window.scrollY !== 0) {
         setIsTopOfPage(false);
       }
-    }
-  window.addEventListener('scroll', handleScroll);
-  return ()=> window.removeEventListener('scroll', handleScroll);
-    
-  }, [])
-  
-
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="app">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} isTopOfPage={isTopOfPage}/>
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
+      />
       <Home setSelectedPage={setSelectedPage} />
-      <Benefits setSelectedPage={setSelectedPage}/>
-      <OurClasses setSelectedPage={setSelectedPage}/>
-      <ContactUs setSelectedPage={setSelectedPage}/>
-      <Footer/>
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
 
-export  {App};
+export { App };
--- src/shared/Types.ts
+++ src/shared/Types.ts
@@ -1,21 +1,27 @@
 export interface BenefitCardType {
-    icon: JSX.Element;
-    title: string;
-    description: string;
-    background: string;
-
+  icon: JSX.Element;
+  title: string;
+  description: string;
+  background: string;
 }
 
 export interface ClassesType {
-    classImage: string,
-    teacherImage: string,
-    teacherName: string,
-    field: string,
-    course: string,
-    duration: string,
-    description?: string,
-    newPrice: string,
-    oldPrice: string,
-    rate: string,
-    stars: string,
+  classImage: string;
+  teacherImage: string;
+  teacherName: string;
+  field: string;
+  course: string;
+  duration: string;
+  description?: string;
+  newPrice: string;
+  oldPrice: string;
+  rate: string;
+  stars: string;
+}
+
+export interface NavigationLinksType {
+  home: string;
+  benefits: string;
+  ourClasses: string;
+  contactUS: string;
 }
